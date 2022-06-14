b42d9f9f453c67bd46e008f1615ffab76e1468a6
Alex Carron <thealexcarron@gmail.com>
2022-06-14 13:10:07 -0500

    Add Menu Items

--- src/menu.js
+++ src/menu.js
@@ -4,7 +4,12 @@ function createMenuPage() {
 	let main_elm = createElement("main"),
 		section_elm = createElement("section"),
 		h2_elm = createElement("h2", "Menu"),
-		menu_container_elm = createElement("div#menu");
+		menu_container_elm = createElement("div#menu"),
+		menu = ["Spaghetti", "Chicken and Cheese Tortillia", "Red beans & Rice"];
+		
+	for (let item of menu) {
+		menu_container_elm.append( createElement("p", item) )
+	}
 	
 	section_elm.append(h2_elm);
 	section_elm.append(menu_container_elm);
