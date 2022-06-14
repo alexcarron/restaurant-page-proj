17ff48755fad80c87dfa14f72850c0d5fcdef64d
Alex Carron <thealexcarron@gmail.com>
2022-06-14 13:13:43 -0500

    Create Contact Page Script

---
+++ src/contact.js
@@ -0,0 +1,24 @@
+import createElement from './element_constructor'
+
+function createMenuPage() {
+	let main_elm = createElement("main"),
+			phone_num_section_elm = createElement("section#phone_num_section"),
+				phone_num_h2_elm = createElement("h2", "Phone Number"),
+				phone_num_p_elm = createElement("p", "(291) 193-0123"),
+			email_section_elm = createElement("section#email_section_elm"),
+				email_h2_elm = createElement("h2", "Email"),
+				email_num_p_elm = createElement("p", "fake@email.com");
+	
+	phone_num_section_elm.append(phone_num_h2_elm);
+	phone_num_section_elm.append(phone_num_p_elm);
+	
+	email_section_elm.append(email_h2_elm);
+	email_section_elm.append(email_num_p_elm);
+	
+	main_elm.append(phone_num_section_elm);
+	main_elm.append(email_section_elm);
+	
+	return main_elm;
+}
+
+export {createMenuPage}
